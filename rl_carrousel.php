eba86d647481bc2fff13c5ab2e9506bcc212fb08
RoxanneLefebvre <102246244+RoxanneLefebvre@users.noreply.github.com>
2022-10-13 11:51:11 -0400

    c9 ajout de function rlc_enqueue

--- rl_carrousel.php
+++ rl_carrousel.php
@@ -11,8 +11,38 @@ Author: Roxanne Lefebvre
 Version: 1.0.0
 */
 
-function genere_boite(){
-$contenu="<div class='carrousel'>Carrousel</div>";
-return $contenu;
+/*
+filemtime()  // retourne en milliseconde le temps de la dernière sauvegarde
+plugin_dir_path() // retourne le chemin du répertoire du plugin
+__FILE__ // le fichier en train de s'exécuter
+wp_enqueue_style() // Intègre le link:css dans la page
+wp_enqueue_script() // intègre le script dans la page
+wp_enqueue_scripts // le hook
+*/
+function rlc_enqueue(){
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+    $version_js  = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+    
+    wp_enqueue_style("rlc_style_carrousel",
+        plugin_dir_url(__file__) . "style.css",
+        array(),
+        $version_css,
+        false  
+    );
+
+    wp_enqueue_script("rlc_js_carrousel",
+        plugin_dir_url(__file__) . "js/carrousel.js",
+        array(),
+        $version_js,
+        false  
+    );
 };
+
+add_action('wp_enqueue_scripts', 'rlc_enqueue');
+
+function genere_boite(){
+    $contenu="<div class='carrousel'>Carrousel</div>";
+    return $contenu;
+};
+
 add_shortcode('rl_carrousel', 'genere_boite');
