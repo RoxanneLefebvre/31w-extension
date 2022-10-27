af7573bbd7fd768f1c268d41ef74a139f11e652f
RoxanneLefebvre <102246244+RoxanneLefebvre@users.noreply.github.com>
2022-10-27 11:11:34 -0400

    ajout de fleche svg

--- rl_carrousel.php
+++ rl_carrousel.php
@@ -44,14 +44,17 @@ function genere_boite(){
     $contenu="
     
     <div class='carrousel'>
-        <button class='btn_close'>X</button>
-        <button class='btn_fleche btn_fleche--gauche'><</button>
-        <button class='btn_fleche btn_fleche--droite'>></button>
-        <figure class='carrousel__figure'></figure>
-        <form class='carrousel__form'></form>
-    </div>
+<button class='btn_close'>X</button>
+
+<svg class='btn_fleche btn_fleche--gauche' data-tn='arrow-left' viewBox='-8.5 0 32 32'><path d='M14 32c-.3 0-.6-.1-.8-.3l-13-15c-.3-.4-.3-.9 0-1.3l13-15c.4-.4 1-.5 1.4-.1.4.4.5 1 .1 1.4L2.3 16l12.4 14.3c.4.4.3 1-.1 1.4-.2.2-.4.3-.6.3z'></path></svg>
+<svg class='btn_fleche btn_fleche--droite' data-tn='arrow-right' viewBox='-8.5 0 32 32'><path d='M1 0c.3 0 .6.1.8.3l13 15c.3.4.3.9 0 1.3l-13 15c-.4.4-1 .5-1.4.1-.4-.4-.5-1-.1-1.4L12.6 16 .2 1.7C-.2 1.3-.1.7.3.3.5.1.8 0 1 0z'></path></svg>
+<figure class='carrousel__figure'></figure>
+<form class='carrousel__form'></form>
+</div>
     ";
     return $contenu;
 };
 
 add_shortcode('rl_carrousel', 'genere_boite');
+
+
